785a714563670243b3c94d80e622fd31deeed91c
Avery Grieve <agrieve@umich.edu>
2021-05-14 15:50:38 -0400

    change case in tsunami.f90, implementing Field type

--- ch08/mod_field.f90
+++ ch08/mod_field.f90
@@ -1,19 +1,27 @@
 module mod_field
 
+    use mod_parallel, only: tile_indices
+
     type :: Field
         character(:), allocatable :: name
-        integer :: dims(2)
+        integer(int32) :: dims(2), lb(2), ub(2)
+        real(real32), allocatable :: data(:,:)
     end type Field
 
+    contains
+
+    type(Field) function field_constrcutor(name, dims) result(res)
+        character(*), intent(in) :: name
+        integer(int32), intent(in) :: dims(2)
+        integer(int32) :: indices(4)
+        res % name = name
+        res % dims = dims
+        indices = tile_indices(dims)
+        res % lb = indices([1, 3])
+        res % ub = indices([2, 4])
+        allocate(res % data(res % lb(1)-1:res % ub(1)+1, &
+                            res % lb(2)-1:res % ub(2)+1))
+        res % data = 0
+    end function field_constrcutor
 end module mod_field
 
-program field_ex
-    
-    use mod_field
-
-    type(Field) :: testfieldplzignore
-    testfieldplzignore = Field('testing',[1,4])
-
-    print *, testfieldplzignore % name
-
-end program field_ex
--- ch08/tsunami.f90
+++ ch08/tsunami.f90
@@ -1,29 +1,29 @@
-PROGRAM TSUNAMI
+program tsunami
 
-    USE ISO_FORTRAN_ENV, ONLY: INT32, REAL32
-    USE MOD_DIFF, ONLY: DIFF => DIFF_CENTERED 
-    USE MOD_PARALLEL, ONLY: TILE_INDICES, TILE_NEIGHBORS
+    use iso_fortran_env, only: int32, real32
+    use mod_diff, only: diff => diff_centered 
+    use mod_parallel, only: tile_indices, tile_neighbors
 
-    IMPLICIT NONE
+    implicit none
 
-    INTEGER(INT32) :: N, I
+    integer(int32) :: n, i
 
-    INTEGER(INT32), PARAMETER :: GRID_SIZE = 100
-    INTEGER(INT32), PARAMETER :: NUM_TIME_STEPS = 5000
+    integer(int32), parameter :: grid_size = 100
+    integer(int32), parameter :: num_time_steps = 5000
     
-    REAL(REAL32), PARAMETER :: dt = 0.02, dx = 1, g = 9.8
+    real(real32), parameter :: dt = 0.02, dx = 1, g = 9.8
     ! the "parameter" identifies constants that cannot be changed
     ! by the program -- also requires assignment on declaration line
    
-    integer(INT32), parameter :: icenter = 25
-    real(REAL32), parameter :: decay = 0.02
+    integer(int32), parameter :: icenter = 25
+    real(real32), parameter :: decay = 0.02
     
     ! declare water height and velocity as allocatable coarrays
     real(real32), allocatable :: h(:)[:], u(:)[:]
     ! declare the gather variable to write data
     real(real32), allocatable :: gather(:)[:]
     ! initializing mean water height stuff
-    REAL(REAL32), allocatable :: hmean(:)
+    real(real32), allocatable :: hmean(:)
     
     integer(int32) :: indices(2), neighbors(2)
     integer(int32) :: left, right
@@ -36,7 +36,7 @@ PROGRAM TSUNAMI
     integer(int32) :: tile_size
 
     if (mod(grid_size, num_images())>0) then
-        error stop 'Error: grid_size must be divisible by number of images'
+        error stop 'error: grid_size must be divisible by number of images'
     endif
 
     neighbors = tile_neighbors()
@@ -61,27 +61,27 @@ PROGRAM TSUNAMI
     ! allocates gather array over the entire domain size
     allocate(gather(grid_size)[*])
     
-    ! Above, the ils/ile variables are the local calculated values. But these values require
+    ! above, the ils/ile variables are the local calculated values. but these values require
     ! the halo cell values in ims and ime.
     ! need to get those values added like h(ime)[left neighbor number] = h(ils)
-    ! Pattern for the parallel time loop is:
-    ! Update halo cells
-    ! Synchronize images
-    ! Solve the step
+    ! pattern for the parallel time loop is:
+    ! update halo cells
+    ! synchronize images
+    ! solve the step
     
     ! provide checks for various input parameters
-    if (GRID_SIZE <= 0) stop 'GRID_SIZE MUST BE > 0'
-    if (dt <= 0) STOP 'TIME STEP dt MUST BE > 0'
-    if (dx <= 0) STOP 'GRID SPACING dx MUST BE > 0'
+    if (grid_size <= 0) stop 'grid_size must be > 0'
+    if (dt <= 0) stop 'time step dt must be > 0'
+    if (dx <= 0) stop 'grid spacing dx must be > 0'
     
-    ! Initialize guassian blob centered @ i=25
+    ! initialize guassian blob centered @ i=25
     do i=is-1, ie+1
         h(i-is+1) = exp(-decay  * (i-icenter)**2)
     enddo
 
-    u = 0 ! Initializes velocity vector to 0s
+    u = 0 ! initializes velocity vector to 0s
     hmean=10 ! initializes height array to 10
-    !Done w/ initial gaussian water height
+    !done w/ initial gaussian water height
 
     gather(is:ie)[1] = h(ils:ile)
     sync all
@@ -90,13 +90,13 @@ PROGRAM TSUNAMI
     ! iterate water solution forward in time
     time_loop: do n=1, num_time_steps
         
-        ! Send this image's halo cells to its neighbors and sync
+        ! send this image's halo cells to its neighbors and sync
         h(ime)[left] = h(ils)
         h(ims)[right] = h(ile)
         sync all
         ! solve and then sync again 
         ! velocity initial guess is all 0, so don't need to pass neighbors around
-        ! during first pass thru loop. After first pass, neighbors are accounted for
+        ! during first pass thru loop. after first pass, neighbors are accounted for
         u = u - (u*diff(u) + g*diff(h))/dx*dt
         sync all
         ! sync the new velocity guesses and then solve height
@@ -112,4 +112,4 @@ PROGRAM TSUNAMI
 
     enddo time_loop    
  
-END PROGRAM TSUNAMI
+end program tsunami
